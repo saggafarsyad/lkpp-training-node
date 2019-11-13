dc502c97404be50ba5e899ab3ada252fc2632c74
Saggaf Arsyad <saggaf@nusantarabetastudio.com>
2019-11-13 17:38:10 +0700

    removed constructor

--- node-ts-codelabs/3-rest-express/todo-api-v2/entity.ts
+++ node-ts-codelabs/3-rest-express/todo-api-v2/entity.ts
@@ -9,13 +9,4 @@ export class ToDoEntity {
   static readonly STATUS_DO = 1
   static readonly STATUS_DOING = 2
   static readonly STATUS_DONE = 3
-
-  constructor(id: number, title: string, content: string) {
-    this.id = id
-    this.title = title
-    this.content = content
-    this.status = ToDoEntity.STATUS_DO
-    this.createdAt = 0
-    this.updatedAt = 0
-  }
 }
